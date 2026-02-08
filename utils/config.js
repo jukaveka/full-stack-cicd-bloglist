edee741845228a718f846bab7cd7d13d9a19a3f1
jukaveka <juhokavekari@hotmail.com>
2026-02-08 19:32:51 +0200

    Remove some logs

--- utils/config.js
+++ utils/config.js
@@ -2,10 +2,6 @@ require("dotenv").config()
 
 const getDatabaseURI = (nodeEnv) => {
   if (nodeEnv === "test") {
-    console.log("test environment")
-    console.log(process.env.TEST_VAR)
-    console.log("env function MONGODB_URI", typeof process.env.TEST_MONGODB_URI)
-    console.log("env", process.env)
     return process.env.TEST_MONGODB_URI
   } else if (nodeEnv === "development") {
     console.log("dev environment")
@@ -21,6 +17,5 @@ const getDatabaseURI = (nodeEnv) => {
 
 const PORT = process.env.PORT
 const MONGODB_URI = getDatabaseURI(process.env.NODE_ENV)
-const TEST_VAR = process.env.TEST_VAR
 
-module.exports = { PORT, MONGODB_URI, TEST_VAR }
+module.exports = { PORT, MONGODB_URI }
